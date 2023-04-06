89ccf37d738b9079f547f8e15fee6c03fd69fb72
Edgar Conrado <edgarconrado23@gmail.com>
2023-04-05 19:52:06 -0600

    Adding Comon Columns model

---
+++ src/main/java/com/belz/bicontrol/viewModel/common/CommonColumnsModel.java
@@ -0,0 +1,11 @@
+package com.belz.bicontrol.viewModel.common;
+
+import lombok.Data;
+
+@Data
+public class CommonColumnsModel {
+    private String header;
+    private String sheetTitle;
+    private String[] columnLabels;
+    private String[] columnNames;
+}
